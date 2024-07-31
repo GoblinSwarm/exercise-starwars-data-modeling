14cbb4d720eb56fd11ac9c2e0325e8edf9bb994a
Daniel Perdomo <136826084+GoblinSwarm@users.noreply.github.com>
2024-07-31 23:12:30 +0000

    Initating the project starwars

--- src/models.py
+++ src/models.py
@@ -7,17 +7,19 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(100), nullable=False)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+
     id = Column(Integer, primary_key=True)
     street_name = Column(String(250))
     street_number = Column(String(250))
@@ -25,8 +27,13 @@ class Address(Base):
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+class Character(Base):
+    __tablename__ = 'character'
+
+    id = Column(Integer, primary_key=True)
+
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
