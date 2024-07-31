6ecccda9b370a0ae3cd2397a7ff0d72caf75cc75
Daniel Perdomo <136826084+GoblinSwarm@users.noreply.github.com>
2024-07-31 23:36:52 +0000

    Falta relacion 1 a 1

--- src/models.py
+++ src/models.py
@@ -1,15 +1,20 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
+from enum import Enum as PyEnum
+
+class Nature(PyEnum):
+    PLANET = 'planet'
+    CHARACTER = 'character'
+
 class User(Base):
     __tablename__ = 'user'
-
     id = Column(Integer, primary_key=True)
     username = Column(String(250), nullable=False)
     firstname = Column(String(250), nullable=False)
@@ -19,21 +24,39 @@ class User(Base):
 
 class Planet(Base):
     __tablename__ = 'planet'
-
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    url = Column(String(250))
+    orbital_period = Column(String(250), nullable=False)
+    gravity = Column(String(250))
+    diameter = Column(Integer)
+    climate = Column(String(250))
 
 class Character(Base):
     __tablename__ = 'character'
-
     id = Column(Integer, primary_key=True)
+    fullname = Column(String(250))
+    birth_year = Column(String(20))
+    hair_color = Column(String(50))
+    height = Column(Integer)
+    weight = Column(Integer)
 
-
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    nature = Column(Enum(Nature), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+    user = relationship(User)
+    planet = relationship(Planet)
+    character = relationship(Character)
 
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+try:
+    render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem generating the diagram")
+    raise e
